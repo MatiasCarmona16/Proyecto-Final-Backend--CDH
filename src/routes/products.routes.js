02dc0e5fb7a6eacba4f747f540bac024eb76347a
Matias Carmona <matias2002carmona@gmail.com>
2024-01-22 00:36:50 -0300

    Agregados en el productmanager

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,26 +1,65 @@
 import { Router } from "express";
+import { ProductManager } from "../models/productManager";
 
+const productManager = new ProductManager('./productos.json')
 
 const routerProd = Router ()
 
 routerProd.get('/', async(req,res) => {
-
+    const {limit} = req.query
+    const prods = await productManager.getProducts()
+    const products = prods.slice(0, limit)
+    res.status(200).send(products)
 })
+
+//-----OBTENER PRODUCTO BY ID-----
 
 routerProd.get('/:id', async (req, res) => {
     const {id} = req.params
+    const prod = await productManager.getProductsById(id)
+
+    if(prod) {
+        res.status(200).send(prod)
+    }else {
+        res.status(404).send('Producto fuera de lista')
+    }
 })
+
+//-----AGREGO UN NUEVO PRODUCTO MEDIANTE POST-----
 
 routerProd.post('/', async(req, res) => {
-
+    const confi = await productManager.addProduct(req.body)
+    if(confi) {
+        res.status(201).send('El producto se agrego')
+    }else {
+        res.status(400).send('El producto se encuentra existente')
+    }
 })
+
+//----------ENVIO EL ID + REQ.BODY---------
 
 routerProd.put('/:id', async (req, res) => {
     const {id} = req.params
+    const confi = await productManager.updateProduct(id, req.body)
+
+    if(confi) {
+        res.status(200).send('El producto se actualizo con exito')
+    }else {
+        res.status(404).send('Producto inexistente')
+    }
 })
+
+//-----ENVIO ID PARA ELIMINAR PRODUCTO-----
 
 routerProd.delete('/:id' , async(req, res) => {
     const {id} = req.params
+    const confi = await productManager.deleteProduct(id)
+
+    if(confi) {
+        res.status(200).send('El producto se elimino con exito')
+    }else {
+        res.status(404).send('Producto inexistente')
+    }
 })
 
 export default routerProd
