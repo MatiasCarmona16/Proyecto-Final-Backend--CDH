6718f52a9074a815d8df0b99714e5edce0afcfdf
Matias Carmona <matias2002carmona@gmail.com>
2024-01-26 18:24:07 -0300

    Agregando handlebars

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import { __dirname } from './path.js'
 import path from 'path'
 import routerCart from './routes/cart.routes.js'
 
+const handlebars = require('express-handlebars')
 const app = express()
 let PORT = 8080
-const uuid4 = require ('uuid4')
 
-//middlewares
+//MOTOR DE PLANTILLA
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname+'/views')
+app.set('view engine', 'handlebars')
+
+//middl
 app.use(express.json()) 
 app.use(express.urlencoded({extended: true}))
 
