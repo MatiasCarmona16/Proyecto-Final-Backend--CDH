dc349dba152729ce99bcf1755f2eef776fe3edee
Matias Carmona <matias2002carmona@gmail.com>
2024-04-01 17:35:39 -0300

    handlbars modi

--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -2,8 +2,12 @@ import { Router } from "express";
 
 const routerHome = Router ()
 
-routerHome.get('/', (req, res) => {
-    res.render('home', {})
+routerHome.get('/', async (req, res) => {
+    try {
+        res.status(200).render('home', { userInfo: req.session.user, titulo: "Home", error: null,})
+    } catch (error) {
+        res.status(500).render("home", { titulo: "Home", error: error,})
+    }
 })
 
 export { routerHome }
